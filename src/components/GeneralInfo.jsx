05577e74f2524374579997ebb93e75b9b3699e05
Tweetsauceyeep <pobusauce12@gmail.com>
2025-10-28 10:14:38 +0800

    removed editing feature for each field

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,29 +1,15 @@
 import { useState } from "react";
 
-function GeneralInfo({generalInfo, setGeneralInfo, handleGeneralSubmit}) {
+function GeneralInfo({generalInfo, setGeneralInfo}) {
 
-  const [isEditing, setIsEditing] = useState(true);
 
   function handleChange(e) {
     const {name, value} = e.target;
     setGeneralInfo({...generalInfo , [name]: value});
 
   }
-
-  // preview feature rather
-  function handleSubmit(e) {
-    e.preventDefault();
-    handleGeneralSubmit();
-    setIsEditing(false);
-  }
-
-  function handleEdit() {
-    setIsEditing(true);
-  }
-
-  if (isEditing) {
     return (
-      <form onSubmit={handleSubmit}>
+      <form >
         <div>
           name:
         </div>
@@ -37,22 +23,9 @@ function GeneralInfo({generalInfo, setGeneralInfo, handleGeneralSubmit}) {
         </div>
         <input  name="phone" type="tel" value={generalInfo.phone} onChange={handleChange}/>
         <div>
-        <button type="submit">Preview</button>
         </div>
       </form>
     )
-  } else {
-    return (
-      <div>
-        <h3>Preview</h3>
-        <p><strong>Name:</strong> {generalInfo.name}</p>
-        <p><strong>Email:</strong> {generalInfo.email}</p>
-        <p><strong>Phone:</strong> {generalInfo.phone}</p>
-        <button onClick={handleEdit}>Edit</button>
-      </div>
-    )
   }
-
-}
 
 export default GeneralInfo
